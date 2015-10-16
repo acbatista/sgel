a02f6ec260f2fccb24337a2de9f9dae07c17f249
Adriano Carvalho <beregueder@gmail.com>
2015-10-15 21:00:13 -0300

    create model user,migration data base, validates the model and add gem pry no GemFile

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+	validates_presence_of :email, :full_name, :location, :password
+	validates_confirmation_of :password
+	validates_length_of :bio, minumum: 20, allow_blank: false
+	validates_uniquenes_of :email
+
+	validate do
+		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015195530) do
+ActiveRecord::Schema.define(version: 20151015200046) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20151015195530) do
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "full_name",  limit: 255
+    t.string   "email",      limit: 255
+    t.string   "password",   limit: 255
+    t.string   "location",   limit: 255
+    t.text     "bio",        limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
