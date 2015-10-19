625f7fc8be8d7a0fa998ac9c772324a29ee45ec3
Adriano Carvalho <beregueder@gmail.com>
2015-10-19 18:14:17 -0200

    Confirmation user from e-mail and Gemfile add server Thin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,25 @@
 class User < ActiveRecord::Base
-	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
-	validates_presence_of :email, :full_name, :location, :password
-	validates_confirmation_of :password
-	validates_length_of :bio, minumum: 20, allow_blank: false
-	validates_format_of :email, with: EMAIL_REGEXP
+  validates_presence_of :email, :full_name, :location, :password
+  validates_confirmation_of :password
+  validates_length_of :bio, minumum: 20, allow_blank: false
+  validates_format_of :email, with: EMAIL_REGEXP
 
-	has_secure_password
+  has_secure_password
+
+  before_create do |user|
+    user.confirmation_token = SecureRandom.urlsafe_base64
+  end
+
+  def confirm!
+    return if confirmed?
+
+    self.confirmed_at = Time.current
+    self.confirmation_token = ''
+  end
+
+  def confirmed?
+    confirmed_at.present?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|pt\-BR/ do
     resources :books
     resources :users
+    resource :confirmation, only: [:show]
   end
 
   get '/:locale' => 'home#index', locale: /en|pt\-BR/
